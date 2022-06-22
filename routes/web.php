42c8c4a4f1cbbb889940389e9ba99fbd4e2ed0a6
ladykakki <greta_calamari@hotmail.it>
2022-06-22 09:50:49 +0200

    inserita fallback

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.home');
-});
+// Route::get('/', function () {
+//     return view('guest.home');
+// });
 
 Auth::routes();
 
@@ -28,5 +28,13 @@ Route::middleware('auth')
        ->group(function(){
               Route::get('/', 'HomeController@index')->name('home');
        });
+
+
+       // ROTTA DI FALLBACK se metti un indirizzo che non esiste vai qua
+
+Route::get("{any?}",function(){
+    return view("guest.home");
+})->where("any",".*");
+
 
 

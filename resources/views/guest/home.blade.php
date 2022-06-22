686c126e8e4f501fac51e226c4d0dc5f8e9f09f8
ladykakki <greta_calamari@hotmail.it>
2022-06-22 10:25:37 +0200

    modifiche admin e aggiunta appmain con colleg. app.vue

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -80,22 +80,8 @@
                 </div>
             @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Boolpress
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+            <div id="root"></div>
         </div>
+        <script src="{{asset('js/front.js')}}" charset="utf-8"></script>
     </body>
 </html>

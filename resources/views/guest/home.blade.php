8dbc3c3841e3ea2bd5a9f6fb162aaac7a8208cd4
ladykakki <greta_calamari@hotmail.it>
2022-06-23 13:51:16 +0200

    ..

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Boolpress</title>
+        <title>Boolpresss</title>
 
         <!-- Fonts -->
         <link href="{{ asset('css/front.css') }}" rel="stylesheet">
